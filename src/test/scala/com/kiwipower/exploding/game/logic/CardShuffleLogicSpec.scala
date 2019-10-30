50cffa8569e18441622bec36d340fc9ba1d3d0fa
power-of-kiwi <fepelan512@7dmail.com>
2019-10-30 14:48:50 +0000

    checking card shuffing logic on narrow range

--- src/test/scala/com/kiwipower/exploding/game/logic/CardShuffleLogicSpec.scala
+++ src/test/scala/com/kiwipower/exploding/game/logic/CardShuffleLogicSpec.scala
@@ -1,5 +1,20 @@
 package com.kiwipower.exploding.game.logic
 
-class CardShuffleLogicSpec {
+import com.kiwipower.exploding.game.domain.Card
+import com.kiwipower.exploding.game.domain.CardType._
+import org.scalatest.{ Matchers, WordSpecLike }
 
+class CardShuffleLogicSpec extends WordSpecLike with Matchers with CardShuffleLogic {
+
+  "card shuffle logic" must {
+
+    "shuffle cards" in {
+      val cardsBefore = List(Card(cardType = DEFUSE), Card(cardType = EXPLOSIVE))
+      val cardsAfter = shuffle(cardsBefore)
+
+      cardsAfter should not equal (cardsBefore)
+      cardsAfter should have size (2)
+    }
+
+  }
 }
